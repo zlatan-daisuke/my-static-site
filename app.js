4f089da5b73f608ca142618eb9cc095e5b1ca8a8
zlatandaisuke <zlatandaisuketheworld@gmail.com>
2026-02-28 17:30:16 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById("msg").textContent = "This is a static site (HTML/CSS/JS).";
+document.getElementById("btn").addEventListener("click", () => {
+  alert("It works!");
+});
+
+// feature-3 change
